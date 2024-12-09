36aea4a1eb504d15872f8eb14ad1a72f2c50479e
venkatesh-r <venkypsd@gmail.com>
2024-12-09 14:55:52 +0100

    connection request schema

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,17 +1,32 @@
 const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-  fromUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-  },
-  toUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-  },
-  status: {
-    type: String,
-    enum: {
-      values: ["ignore", "accepted", "interested", "rejected"],
-      message: `{VALUE} is incorrect status type`,
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "accepted", "interested", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
     },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const connectionRequestModel = new mongoose.model(
+  "connectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = connectionRequestModel;
