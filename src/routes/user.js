c3655245cee6378972f707bd7a9c3c0be707cd82
venkatesh-r <venkypsd@gmail.com>
2025-01-10 15:03:28 +0100

    feed API skip and limit

--- src/routes/user.js
+++ src/routes/user.js
@@ -52,6 +52,13 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 1;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
     // Find all connection requests
     const connectionRequest = await connectionRequestModel
       .find({
@@ -71,7 +78,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USERSAFE_DATA);
+    })
+      .select(USERSAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.json({ message: users });
   } catch (err) {
